71c7ebfb518dba8ad71c115a36293b5da531dd94
Adriasu <adriana.suarez@students.gammatech.school>
2024-05-04 00:14:19 +0200

    ejercicio 3 de array terminado

--- array.js
+++ array.js
@@ -1,10 +1,30 @@
-// EJERCICIO 1
+// // EJERCICIO 1
 
-const firstArray = [];
+// const firstArray = [];
 
-// EJERCICIO 2 
+// // EJERCICIO 2
 
-const secondArray = [3, "nana", true];
+// const secondArray = [3, "nana", true];
 
-// EJERCICIO 3
+// // EJERCICIO 3.1
+
+// const animalArray = [];
+
+// animalArray.push("dog");
+// animalArray.push("cat");
+// animalArray.unshift("fish");
+// animalArray.unshift("lizard");
+
+// animalArray.pop();
+// animalArray.shift();
+
+// console.log(animalArray.length);
+
+// // EJERCICIO 3.2
+
+// const classNames = ["Gabriela", "Adriana", "Jesus Raad", "Andrea", "Javmir", "Roger", "Jesus Aleman"];
+// console.log(classNames[1]); 
+
+// EJERCICIO 4
+
 
