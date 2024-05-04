d107e9fc5fead517a9f30b725ddf45b09c7ce2b5
Adriasu <adriana.suarez@students.gammatech.school>
2024-05-05 01:42:14 +0200

    ejercicio 3 objetos terminado

--- objetos.js
+++ objetos.js
@@ -1,16 +1,31 @@
-// // EJERCICIO 1
+// EJERCICIO 1
 
-// const citizen1 = {
-//   name: "Adriana",
-//   lastName: "Suarez",
-//   age: 33,
-// };
+const citizen1 = {
+  name: "Adriana",
+  lastName: "Suarez",
+  age: 33,
+};
 
-// // EJERCICIO 2
+// EJERCICIO 2
 
-// citizen1.city = "Bucaramanga"
-// citizen1.age = 34
+citizen1.city = "Bucaramanga";
+citizen1.age = 34;
 
-// console.log(citizen1);
+console.log(citizen1);
 
-// EJERCICIO 3 
+// EJERCICIO 3
+
+function checkCitizenship(objeto) {
+  for (let key in citizen1) {
+    if (key === "age") {
+      if (citizen1[key] > 18) {
+        return (citizen1.canVote = true);
+      } else return (citizen1.canVote = false);
+    }
+  }
+}
+
+checkCitizenship(citizen1);
+console.log(citizen1);
+
+
