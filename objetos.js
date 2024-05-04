96a52605c7f53f43f03e6f405b73a2dc20733fcf
Adriasu <adriana.suarez@students.gammatech.school>
2024-05-05 00:30:59 +0200

    Ejercicio 2 objetos terminado

--- objetos.js
+++ objetos.js
@@ -1,7 +1,16 @@
-// EJERCICIO 1
+// // EJERCICIO 1
 
-const citizen1 = {
-  name: "Adriana",
-  lastName: "Suarez",
-  age: 33,
-};
+// const citizen1 = {
+//   name: "Adriana",
+//   lastName: "Suarez",
+//   age: 33,
+// };
+
+// // EJERCICIO 2
+
+// citizen1.city = "Bucaramanga"
+// citizen1.age = 34
+
+// console.log(citizen1);
+
+// EJERCICIO 3 
